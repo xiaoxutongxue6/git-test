3d72c63b0aeb6e05c8fe1785b70f59dc87f653a2
飞奔的奶牛 <2258526873@qq.com>
2022-12-20 09:44:59 +0000

    gitee2
    Signed-off-by: 飞奔的奶牛 <2258526873@qq.com>

--- src/main/java/com/xiaoheizi/GitTest.java
+++ src/main/java/com/xiaoheizi/GitTest.java
@@ -10,6 +10,6 @@ public class GitTest {
         System.out.println("李白");
         System.out.println("push");
          System.out.println("pull");
-        System.out.println("gitee版本一");
+        System.out.println("gitee版本2");
     }
 }
