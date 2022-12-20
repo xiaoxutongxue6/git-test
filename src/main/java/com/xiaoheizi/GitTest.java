7cba31c14665932378b4b24aa05d756954a30b36
xjm <2258526873@qq.com>
2022-12-20 14:09:55 +0800

    hot-fix1.0

--- src/main/java/com/xiaoheizi/GitTest.java
+++ src/main/java/com/xiaoheizi/GitTest.java
@@ -5,5 +5,6 @@ public class GitTest {
         System.out.println("心若有所向往，何惧道阻且长");
         System.out.println("hello Java");
         System.out.println("放飞梦想");
+        System.out.println("及你太美");
     }
 }
