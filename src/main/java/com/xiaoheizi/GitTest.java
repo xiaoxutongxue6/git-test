ac471407597b4aecaea281ec0f2d6c5d7092c202
xjm <2258526873@qq.com>
2022-12-20 13:55:44 +0800

    git2.0

--- src/main/java/com/xiaoheizi/GitTest.java
+++ src/main/java/com/xiaoheizi/GitTest.java
@@ -3,5 +3,6 @@ package com.xiaoheizi;
 public class GitTest {
     public static void main(String[] args){
         System.out.println("心若有所向往，何惧道阻且长");
+        System.out.println("hello Java");
     }
 }
